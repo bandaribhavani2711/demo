727da81e39b272dfb1f7dd908b22ba12c653cd07
bhavani.bandari@zelis.com <bhavani.bandari@zelis.com>
2026-06-19 05:35:45 +0000

    build

--- models/2.Stage/stg_demo.sql
+++ models/2.Stage/stg_demo.sql
@@ -24,21 +24,21 @@ INNER JOIN {{ source('demo_sources', 'PRODUCTS') }} p
 {{ config(materialized='view') }}
 
 SELECT
-    c.CUSTOMER_ID,
-    c.CUSTOMER_NAME,
-    c.PRODUCT_ID,
-    c.PURCHASE_DATE,
-    c.QUANTITY,
-    c.TOTAL_AMOUNT,
-    c.CITY,
+    c.customer_id,
+    c.customer_name,
+    c.product_id,
+    c.purchase_date,
+    c.quantity,
+    c.total_amount,
+    c.city,
 
-    p.PRODUCT_NAME,
-    p.CATEGORY,
-    p.BRAND,
-    p.UNIT_PRICE,
-    p.STOCK_QUANTITY,
-    p.CREATED_DATE
+    p.product_name,
+    p.category,
+    p.brand,
+    p.unit_price,
+    p.stock_quantity,
+    p.created_date
 
-FROM {{ source('demo_sources', 'CUSTOMER') }} c
-INNER JOIN {{ source('demo_sources', 'PRODUCTS') }} p
+FROM {{ source('demo_sources', 'CUSTOMER') }} AS c
+INNER JOIN {{ source('demo_sources', 'PRODUCTS') }} AS p
     ON c.PRODUCT_ID = p.PRODUCT_ID
